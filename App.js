0fe5035bea03fc4cb90cf8a0f54705fc37f95faf
Amelie NGUYEN <amelienguyen@MacBook-Pro-de-Reset.local>
2021-12-07 23:14:57 +0100

    done

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, Button} from 'react-native';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
 
 export default function App() {
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+  const [errorMsg, setErrorMsg] = useState(null);
+  const setBack = () => {
+    setLatitude('0')
+    setLongitude('0')
+  }
+  const getPosition = useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLatitude(location.coords.latitude);
+      setLongitude(location.coords.longitude);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text> Longitude : {longitude} </Text>
+      <Text> Latitude : {latitude} </Text>
+      <Button onPress={getPosition}style={styles.button} title={'Get Location'}></Button>
+      <Button onPress={() => setBack()} style={styles.button} title={'Reset Location'}></Button>
     </View>
   );
 }
@@ -18,4 +47,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignContent: 'center',
+    marginBottom: 20
+  },
+  text: {
+    paddingBottom: 10
+  }
 });
